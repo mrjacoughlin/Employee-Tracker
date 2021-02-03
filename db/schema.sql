4f7f2e254b6f28bad340e8f76460c0297605563c
James Coughlin <mrjacoughlin@gmail.com>
2021-02-02 22:31:51 -0500

    main prompt function

--- db/schema.sql
+++ db/schema.sql
@@ -27,3 +27,4 @@ CREATE TABLE employee (
     manager_id INT UNSIGNED,
     INDEX man_ind (manager_id),
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
---
+++ index.js
@@ -0,0 +1,73 @@
+const { prompt } = require("inquirer");
+const { INITIALLY_DEFERRED } = require("sequelize/types/lib/deferrable");
+const { values } = require("sequelize/types/lib/operators");
+const db = require("./db");
+require("console.trable");
+
+function mainPrompts() {
+  const { choice } = prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        {
+          name: "View all employees",
+          value: "VIEW_EMPLOYEES",
+        },
+        {
+          name: " View all employees by Department",
+          value: "VIEW ALL EMPLOYEES_BY_DEPARTMENT",
+        },
+        {
+          name: "View all employees by manager",
+          value: "VIEW_EMPLOYEES_BY+MANAGER",
+        },
+        {
+          name: "Add employee",
+          value: "ADD_EMPLOYEE",
+        },
+        {
+          name: "Remove employee",
+          value: "REMOVE_EMPLOYEE",
+        },
+        {
+          name: "Update Employee Role",
+          value: "UPDATE_EMPLOYEE_ROLE",
+        },
+        {
+          name: "Update Employee Manager",
+          value: "UPDATE_EMPLOYEE_MANAGER",
+        },
+        {
+          name: "view all roles",
+          value: "VIEW_ROLES",
+        },
+        {
+          name: "Add Roles",
+          value: "ADD_ROLES",
+        },
+        {
+          name: "Remove roles",
+          value: "REMOVE_ROLES",
+        },
+        {
+          name: "view all departments",
+          value: "VIEW_DEPARTMENT",
+        },
+        {
+          name: "add department",
+          value: "ADD_DEPARTMENT",
+        },
+        {
+          name: "remove department",
+          value: "REMOVE_DEPARTMENT",
+        },
+        {
+          name: "quit",
+          value: "QUIT",
+        },
+      ],
+    },
+  ]);
+}
