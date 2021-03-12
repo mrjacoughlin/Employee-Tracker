1fc3ffc775df8dbae5407154eb1b28f6063f33d6
James Coughlin <mrjacoughlin@gmail.com>
2021-03-12 12:26:52 -0500

    employee values

--- db/seed.sql
+++ db/seed.sql
@@ -21,4 +21,12 @@ VALUES ('Sales Lead', 100000, 1),
 INSERT INTO employee
 (first_name, last_name, role_id, manager_id)
 VALUES 
+ ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
 
